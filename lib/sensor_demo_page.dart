a5cfe98a7a6c26fdaafaad0bb0812043324a5b0a
Akshadabobade <akshadabobade4912@gmail.com>
2025-03-08 02:08:08 +0530

    commited

--- lib/sensor_demo_page.dart
+++ lib/sensor_demo_page.dart
@@ -15,11 +15,11 @@ class _SensorDemoPageState extends State<SensorDemoPage> {
   AccelerometerEvent? _accelerometerEvent;
   StreamSubscription<AccelerometerEvent>? _accelerometerSubscription;
   @override
-void initState(){
+  void initState(){
     super.initState();
     _accelerometerSubscription =accelerometerEventStream().listen((event){
       print(event);
-      
+
       setState(() {
         _accelerometerEvent=event;
 
@@ -27,7 +27,7 @@ void initState(){
 
     });
 
-    
+
   }
 
   @override
@@ -36,36 +36,36 @@ void initState(){
       appBar: AppBar(title:Text("Accelerometer Demo")),
       body: Center(
         child: SafeArea(
-            child: Column(
-              crossAxisAlignment:CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-          Text("Accelerometer value",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
+          child: Column(
+            crossAxisAlignment:CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Accelerometer value",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Text("x:${_accelerometerEvent?.x.toStringAsFixed(3)}",
+                style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(height: 20.0),
+              Text("y:${_accelerometerEvent?.y.toStringAsFixed(3)}",
+                style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(height: 20.0),
+              Text("z:${_accelerometerEvent?.z.toStringAsFixed(3)}",
+                style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(height: 20.0),
+              Text(
+                "Move your device around to see the accelerometer values change!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal),
+
+              ),
+            ],
           ),
-                SizedBox(height: 20.0),
-                Text("x:${_accelerometerEvent?.x.toStringAsFixed(3)}",
-                  style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                ),
-
-                SizedBox(height: 20.0),
-                Text("y:${_accelerometerEvent?.y.toStringAsFixed(3)}",
-                  style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                ),
-
-                SizedBox(height: 20.0),
-                Text("z:${_accelerometerEvent?.z.toStringAsFixed(3)}",
-                  style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                ),
-
-                SizedBox(height: 20.0),
-          Text(
-            "Move your device around to see the accelerometer values change!",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal),
-
-          ),
-              ],
-            ),
         ),
       ),);
   }
